e5344921de74bb459cbe47c73528eefcb602998c
André Vinícius Leicht <andreerdnaleicht@gmail.com>
2022-10-06 16:08:37 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [numero, setNumero] = useState(0);
+
+  function aumentar() {
+    setNumero(numero + 1);
+  }
+  function diminuir(){
+    setNumero(numero - 1);
+  }
+  function zerar(){
+    setNumero(numero - numero);
+  }
+
+  return (
+    <div className="App">
+     <h1>Contador</h1>
+     <p>{numero}</p>
+     <button onClick={aumentar}>Mais</button>
+     <button onClick={zerar}>Zerar</button>
+     <button onClick={diminuir}>Menos</button>
+    </div>
+  );
+}
+
+export default App;
